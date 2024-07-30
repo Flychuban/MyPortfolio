0670e9255e08a42e90f97ca3db918c0f89fcb38b
Flychuban <kaloanas07@gmail.com>
2024-07-30 23:27:05 +0300

    add footer

---
+++ website_react/src/sections/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+  return (
+    <section id='footer' className={styles.container}>
+        <p> &copy; 2024 Kaloyan Anastasov. <br/> All rights reserved.</p>
+    </section>
+  )
+}
+
+export default Footer

0dc67bb914855eef7f75dfd913e26ed4557041d1
Flychuban <kaloanas07@gmail.com>
2024-08-01 15:59:44 +0300

    add code for sending mail whne form is submited

--- website_react/src/sections/Contact/Contact.jsx
+++ website_react/src/sections/Contact/Contact.jsx
@@ -4,14 +4,14 @@ function Contact() {
   return (
     <section id='contact' className={styles.container}>
         <h1 className='sectionTitle'>Contact</h1>
-        <form action="">
+        <form action="https://formspree.io/f/mwpeobgg" method='post'>
             <div className='formGroup'>
                 <label htmlFor="name" hidden>Name</label>
                 <input type="text" name='name' id='name' placeholder='Name' required/>
             </div>
             <div className='formGroup'>
                 <label htmlFor="email" hidden>Email</label>
-                <input type="text" name='email' id='email' placeholder='Email' required/>
+                <input type="email" name='email' id='email' placeholder='Email' required/>
             </div>
             <div className='formGroup'>
                 <label htmlFor="message" hidden>Message</label>
